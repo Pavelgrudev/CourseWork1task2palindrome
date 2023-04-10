10748971087e4f8b1d4e0644dd98af0d4cc9a14a
Павел <Pavel-grudev@yandex.ru>
2023-04-10 17:28:13 +0300

    Branch1 ++

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "llццддддццll";
+        String s = "l33332223333yl";
+        System.out.println(isPalindrome(s) ? "Это Палиндром" : "это не Палиндром");}
+        private static boolean isPalindrome(String s) {
+            if (s.length() == 1 || s.length() == 0) {
+                return true;}
+            if (s.charAt(0) == s.charAt(s.length() - 1)) {
+                return isPalindrome(s.substring(1, s.length() - 1));
+            }
+        return  false;}
+}
 
-        System.out.println(isPalindrome(s) ? "Это палиндром" : "это не Палиндром");}
-    private static boolean isPalindrome(String s) {
-      if(s.length() == 1 || s.length() == 0) {
-        return true;}
-     if(s.charAt(0) == s.charAt(s.length() - 1)) {
-         return  isPalindrome(s.substring(1,s.length()-1));}
-return  false;
-}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
