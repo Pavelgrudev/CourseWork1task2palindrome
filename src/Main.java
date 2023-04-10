9814e111fe0f5ed59dd725d47897c755a0145f87
Павел <Pavel-grudev@yandex.ru>
2023-04-10 16:50:02 +0300

    ++

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        String s = "llццддддццll";
+
+        System.out.println(isPalindrome(s) ? "Это палиндром" : "это не Палиндром");}
+    private static boolean isPalindrome(String s) {
+      if(s.length() == 1 || s.length() == 0) {
+        return true;}
+     if(s.charAt(0) == s.charAt(s.length() - 1)) {
+         return  isPalindrome(s.substring(1,s.length()-1));}
+return  false;
+}}
+
+
